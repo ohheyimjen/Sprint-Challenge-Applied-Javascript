5a136d3bef450a9ced47368abd817d4189f9f441
ohheyimjen <jendority@gmail.com>
2019-03-15 10:56:47 -0700

    Fixed Tabs file, now complete

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,24 +3,24 @@ class TabLink {
     // assign this.tabElement to the tabElement DOM reference
    
     this.tabElement = tabElement;
-    
+    console.log(tabElement.dataset);
     // Get the `data-tab` value from this.tabElement and store it here
-    this.tabData = document.querySelector(`.tabs[data-tab="${this.tabElement}"]`); 
+    this.tabData = tabElement.dataset.tab;
 
     
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
     
     //<- Delete this comment block when you work on the if statement
     // Check to see if this.tabData is equal to 'all'
-     if('all' === true){
+     if(this.tabData === 'all'){
       // If `all` is true, select all cards regardless of their data attribute values
-      this.cards = document.querySelectorAll('.cards');
+      this.cards = document.querySelectorAll('.card');
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      this.cards = this.tab; 
+      this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`); 
     }
     //<- Delete this comment block when you work on the if statement
-
+  console.log(this.cards);
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
       this.cards = Array.from(this.cards).map(card => new TabCard(card));
 
